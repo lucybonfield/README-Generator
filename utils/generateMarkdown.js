3565d055a998f384f6571266df2d2acb65e008b3
bonfield2l <lucy.bonfield2@paddypowerbetfair.com>
2023-12-20 15:10:08 +0000

    generateMarkdown function to create format of readme based on user input

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,18 @@
-// function to generate markdown for README
+// function to generate markdown content based on user input
+
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `
+# ${data.title}
+## Description
+${data.description}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Credits
+${data.credits}
+## License
+This project is licensed under the ${data.license} license.
+  `;
 }
-
 module.exports = generateMarkdown;
