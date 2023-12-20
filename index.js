75656ab7b34c8e5752caa60484e9a118167fcfcc
bonfield2l <lucy.bonfield2@paddypowerbetfair.com>
2023-12-20 15:03:47 +0000

    added questions array, and functions to write the file and initalise it

--- index.js
+++ index.js
@@ -5,17 +5,33 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description explaining the what, why, and how of your project:',
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  return fs.writeFileSync(fileName, data);
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions)
+    .then((answers) => {
+      const readmeContent = generateMarkdown(answers);
+      const outputPath = path.join(process.cwd(), 'README.md');
+      writeToFile(outputPath, readmeContent);
+      console.log(`README.md successfully generated at ${outputPath}`);
+    })
+    .catch((error) => console.error(error));
 }
-
 // function call to initialize program
 init();
